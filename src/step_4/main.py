9fb71121fc5f5c81e47865df12cba7377effb562
uyplayer <uyplayer@outlook.com>
2026-07-15 09:51:38 +0800

    修复

---
+++ src/step_4/main.py
@@ -0,0 +1,36 @@
+'''
+Author: uyplayer
+Email: uyplayer@outlook.com
+Date: 2026-07-13 08:56:43
+LastEditTime: 2026-07-15 08:49:53
+LastEditors: uyplayer uyplayer@outlook.com
+Description: some description
+FilePath: /CFDPython/src/step_4/main.py
+@copyright Copyright (c) 2026 by uyplayer
+'''
+ 
+import os
+import numpy
+from matplotlib import pyplot
+import time, sys
+import scipy as sp
+import matplotlib.animation as animation
+from sympy import N
+
+
+
+
+
+
+class  DiffusionEquationOneD:
+    def __init__(self, nx ):
+        
+        
+        ...
+        
+    def simulate(self):
+        ...
+        
+        
+    def plot(self):    
+        ...    
