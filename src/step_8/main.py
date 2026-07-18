86d59854aaaed6e6ed15608f045d2a30748fb5bc
uyplayer <uyplayer@outlook.com>
2026-07-18 18:07:27 +0800

    ``` chore(lessons): 更新Python版本号为占位符
    由于环境配置问题，将Python版本从具体版本号3.9.25
    更新为占位符3.9.undefined，以避免版本不匹配导致的问题。
    ```

---
+++ src/step_8/main.py
@@ -0,0 +1,20 @@
+ 
+
+
+
+
+
+
+
+
+
+class BurgersEquation2D:
+    def __init__(self ):
+        pass 
+         
+        
+    def simulate(self):   
+        pass 
+    
+    def plot(self):
+        pass 
