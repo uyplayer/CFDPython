70640ea0c122cdffc8dab63f965bbc97b79d524d
uyplayer <uyplayer@outlook.com>
2026-07-18 14:51:44 +0800

    ``` fix(lessons): 修复notebook内核配置中的版本号和显示名称
    更新07_Step_5.ipynb中的Python版本号从"3.9.undefined"到"3.9.-1"
    
    更新08_Step_6.ipynb中的内核显示名称从"Python 3"到"cfdpython (3.9.x)"
    
    更新08_Step_6.ipynb中的Python版本号从"3.5.2"到"3.9.undefined"
    ```

---
+++ src/step_7/main.py
@@ -0,0 +1,29 @@
+'''
+Author: uyplayer
+Email: uyplayer@outlook.com
+Date: 2026-07-18 12:10:49
+LastEditTime: 2026-07-18 14:50:38
+LastEditors: uyplayer
+Description: some description
+FilePath: /CFDPython/src/step_7/main.py
+@copyright Copyright (c) 2026 by uyplayer
+'''
+
+
+
+class TwoDDiffusion:
+    
+    def __init__(self ):
+        pass 
+        
+        
+    def simulate(self):   
+        pass 
+    
+    
+    def plot(self):
+        pass  
+
+
+
+
